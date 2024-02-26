8248c4f2e2b9bcbcbc0daeeaf1c6779afea52b15
revilum <revilum.hi@gmail.com>
2024-02-26 17:46:41 +0100

    another update

--- website/src/index.js
+++ website/src/index.js
@@ -5,13 +5,26 @@ const REQUEST_PARAMS = {
     south: 53.4985902,
     east: 11.3579961,
     opt: {
-        //results: 256,
+        products: {
+            suburban: true, //S-Bahn Hambugr
+            subway: true, //U-Bahn Hamburg
+            bus: false, //Linienbusse
+            ferry: true, //Fähren
+            regional: true, //Alle RE/RB/AKN
+            onCall: false, //OnCall Busse.
+            national: true, //Intercity(IC)/Eurocity(EC)
+            nationalExpress: true, //Intercity Express(ICE)
+            interregional: true //Das Sylt-Shuttle Plus Meme. Heisst irgendwie A10 oder so kp
+        },
+        results: 512,
         polylines: true,
         subStops: false,
         entrances: false,
-        language: 'de'
+        language: 'en'
     }
 }
+
+const MARKERS = {}
 
 const map = L.map('map', {
     center: [54.3126897, 10.129182],
@@ -29,7 +42,7 @@ const tmpIcon = L.icon({
 })
 
 async function updateData() {
-    let tmp = (await (await fetch(API_URL + "/radar", {
+    let data = (await (await fetch(API_URL + "/radar", {
         method: "POST",
         body: JSON.stringify(REQUEST_PARAMS),
         headers: {
@@ -37,12 +50,33 @@ async function updateData() {
         }
     })).json())
 
-    console.log(JSON.stringify(tmp.movements))
-
-    tmp.movements.forEach(obj => {
-        L.marker([obj.location.latitude, obj.location.longitude], {icon: tmpIcon}).addTo(map)
+    /*MARKERS.forEach((obj) => {
+        map.removeLayer(obj)
     })
+
+    MARKERS.length = 0*/
+    const currentUpdateTime = Date.now().toString();
+    data.movements.forEach(train => {
+        if (MARKERS[train.tripId] !== undefined) {
+            MARKERS[train.tripId].marker.setLatLng(new L.LatLng(train.location.latitude, train.location.longitude))
+            MARKERS[train.tripId].lastUpdate = currentUpdateTime
+        } else {
+            MARKERS[train.tripId] = {
+                marker: L.marker([train.location.latitude, train.location.longitude], {icon: tmpIcon})
+                    .bindPopup(train.line.name + " nach " + train.direction)
+                    .addTo(map),
+                lastUpdate: currentUpdateTime
+            }
+        }
+    })
+
+    for (const [trip, marker] of Object.entries(MARKERS)) {
+        if (marker.lastUpdate !== currentUpdateTime) {
+            map.removeLayer(marker.marker)
+            delete MARKERS[trip]
+        }
+    }
 }
 
-updateData().then(()=> console.log("updated data"))
+updateData().then()
 setInterval(updateData, 10000)
